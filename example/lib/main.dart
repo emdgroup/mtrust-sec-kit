382744ed9b82ae836281cff2be83e9aaf46ff559
Yannick Stolle <yannick.stolle@merckgroup.com>
2025-01-14 15:31:11 +0100

    refactor: clean up example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:typed_data';
-
+import 'package:example/virtual_strategy.dart';
 import 'package:flutter/material.dart';
 import 'package:mtrust_sec_kit/mtrust_sec_kit.dart';
-
-import 'package:mtrust_urp_virtual_strategy/mtrust_urp_virtual_strategy.dart';
 
 import 'package:liquid_flutter/liquid_flutter.dart';
 
@@ -27,26 +24,6 @@ void main() {
   ));
 }
 
-extension PrependLength on Uint8List {
-  Uint8List prependLength() {
-    return Uint8List.fromList(_int16ToBytes(length) + this);
-  }
-
-  Uint8List _int16ToBytes(int value) {
-    if (value < -32768 || value > 32767) {
-      throw ArgumentError('Value must be between -32768 and 32767.');
-    }
-
-    final bytes = Uint8List(2);
-
-    // Store the two bytes in little-endian order
-    bytes[0] = (value >> 8) & 0xFF; // Upper byte
-    bytes[1] = value & 0xFF; // Lower byte
-
-    return bytes;
-  }
-}
-
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
@@ -57,33 +34,9 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   bool _canDismiss = false;
 
-  final _virtualStrategy = UrpVirtualStrategy((UrpRequest request) async {
-    final payload = UrpSecCommandWrapper.fromBuffer(request.payload);
-    final result = switch (payload.deviceCommand.command) {
-      (UrpSecCommand.urpSecPrime) => UrpResponse(),
-      (UrpSecCommand.urpSecStartMeasurement) => UrpResponse(
-          payload: UrpSecMeasurement(
-            nonce: 12,
-            readerId: "foo",
-            signature: "bar",
-          ).writeToBuffer(),
-        ),
-      _ => switch (payload.coreCommand.command) {
-          (UrpCommand.urpOff) => UrpResponse(),
-          _ => throw Exception("Command not supported"),
-        },
-    };
-
-    await Future.delayed(const Duration(seconds: 2));
-
-    return result;
-  });
-
-  //late final _bleStrategy = UrpBleStrategy();
-
   @override
   void initState() {
-    _virtualStrategy.createVirtualReader(FoundDevice(
+    virtualStrategy.createVirtualReader(FoundDevice(
       name: "SEC-000123",
       type: UrpDeviceType.urpSec,
       address: "00:00:00:00:00:00",
@@ -117,8 +70,8 @@ class _MainAppState extends State<MainApp> {
                 ldSpacerL,
                 SecModalBuilder(
                   canDismiss: _canDismiss,
-                  strategy: _virtualStrategy,
-                  payload: "",
+                  strategy: virtualStrategy,
+                  payload: "<example payload>",
                   onDismiss: () {
                     debugPrint("Dismissed");
                   },

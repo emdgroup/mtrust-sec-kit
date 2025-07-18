ebab5d7a6970b6e8e76a4d3d52e2815185726c5f
Joshua Wellbrock <joshuawellbrock@9353dcd1-d5bb-4a23-a6ec-fe3367c6dea9.fritz.box>
2025-07-18 08:57:39 +0200

    chore: add missing trailing comma

--- lib/src/ui/l10n/sec_locale.dart
+++ lib/src/ui/l10n/sec_locale.dart
@@ -63,7 +63,7 @@ import 'sec_locale_en.dart';
 /// property.
 abstract class SecLocalizations {
   SecLocalizations(String locale)
-      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -86,16 +86,16 @@ abstract class SecLocalizations {
   /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('de'),
-    Locale('en')
+    Locale('en'),
   ];
 
   /// No description provided for @successfullyVerified.
@@ -276,8 +276,9 @@ SecLocalizations lookupSecLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-      'SecLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-      'an issue with the localizations generation tool. Please file an issue '
-      'on GitHub with a reproducible sample app and the gen-l10n configuration '
-      'that was used.');
+    'SecLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
 }
